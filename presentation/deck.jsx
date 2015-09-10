7795bb74681dfa9aa01376ee203337fcd8848ee1
Jakub Dutkowski <jdutkowski@freeportmetrics.com>
2015-09-10 14:16:17 +0200

    added more slides

--- presentation/deck.jsx
+++ presentation/deck.jsx
@@ -39,7 +39,7 @@ export default class extends React.Component {
             <ListItem><Appear fid="2">Intro to Flux</Appear></ListItem>
             <ListItem><Appear fid="3">Flux libraries</Appear></ListItem>
             <ListItem><Appear fid="4">Why use it?</Appear></ListItem>
-            <ListItem><Appear fid="5">Can I use it with AngularJS?</Appear></ListItem>
+            <ListItem><Appear fid="5">Do I have to leave AngularJS?</Appear></ListItem>
           </List>
         </Slide>
         <Slide transition={["spin"]} bgColor="primary" textColor="tertiary">
@@ -55,6 +55,29 @@ export default class extends React.Component {
             lang="javascript"
             source={require("raw!./example2.example")}
             margin="20px auto"/>
+        </Slide>
+        <Slide transition={["spin"]} bgColor="primary" textColor="tertiary">
+          <Heading>Main advantages</Heading>
+          <List>
+            <ListItem><Appear fid="1">Low complexity</Appear></ListItem>
+            <ListItem><Appear fid="2">Flexibility</Appear></ListItem>
+            <ListItem><Appear fid="3">Easy to use stuff like npm, Browserify (modularity)</Appear></ListItem>
+            <ListItem><Appear fid="3">'Native' jsx</Appear></ListItem>
+            <ListItem><Appear fid="5">Virtual DOM</Appear></ListItem>
+          </List>
+        </Slide>
+        <Slide transition={["spin"]} bgColor="primary" textColor="tertiary">
+          <Heading>Virtual DOM</Heading>
+          <Text>Fast in-memory rendering</Text>
+          <Text>Propagate changes to DOM with diff</Text>
+          <Text>Ability to render server side with no heavy browser DOM</Text>
+        </Slide>
+        <Slide>
+          <Heading>Where are my 'M' and 'C'?</Heading>
+        </Slide>
+        <Slide>
+          <Text>React.js is not really a 'full' framework - more like a view library</Text>
+          <Text>But Facebook </Text>
         </Slide>
       </Deck>
     );
